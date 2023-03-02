e36566b3a5769d7c58bcdac3b1a7cc12813372b8
Sergi Romeu <romeuhd@gmail.com>
2023-03-02 20:45:38 +0100

    chore: :art: add chakraui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,28 @@
-import "../styles/reset.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import localFont from "next/font/local";
+
+import "../styles/reset.css";
 
 const starJediFont = localFont({
   src: "../../public/fonts/Starjedi.ttf",
   variable: "--font-starjedi",
 });
 
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({ config });
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${starJediFont.variable} font-sans`}>
-      <Component {...pageProps} />;
-    </main>
+    <ChakraProvider resetCSS={false} theme={theme}>
+      <main className={`${starJediFont.variable} font-sans`}>
+        <Component {...pageProps} />;
+      </main>
+    </ChakraProvider>
   );
 }
